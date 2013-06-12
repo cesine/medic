d1a18bf79c448cd7c1bf74d2c6f0c4f8699f4663
Ryan Willoughby <ryan.willoughby@adobe.com>
2013-06-11 17:30:28 -0700

    [http] initial http api commit

--- run.js
+++ run.js
@@ -28,32 +28,57 @@ shell.mkdir(temp);
 var default_spec = 'mobile_spec';
 var queue = new q();
 
-console.log('[MEDIC] Running.');
+if (argv.server) {
+    // fire up a server, n start waitin for requests
 
-// main initializes the builder, updating build tools
-var init = require('./src/builder/init');
+    console.log('[MEDIC] Running web server.');
 
-// the runner, well, runs
-var runner = require('./src/builder/runner');
+    var app = require("express")();
 
-init(config, function(initial_queue) {
+    app.use(app.router); //use both root and other routes below
 
-    function queueJob(job) {
-        for (var i in job) {
-            if (job.hasOwnProperty(i)) {
-                var spec = job[i].spec ? job[i].spec.name : default_spec;
-                var git_url = job[i].spec ? job[i].spec.git : null;
-                var entry = job[i].spec ? job[i].spec.entry : config.app.entry;
-                job[i].builder = require('./src/builder/builder')(spec, entry, false, git_url);
-                queue.push(job);
+    // redirect db queries to remote couchdb
+    app.get("/*", function(req, response){ 
+        // so secure.
+        var s = shell.exec('whoami');
+        response.send();
+    });
+
+    var address = "127.0.0.1",
+        port = 8081;
+
+    console.log("Listening to http://" + address + ":" + port + "/");
+    app.listen(port, address);
+
+
+} else {
+
+    console.log('[MEDIC] Running.');
+    // clones the repos
+    var init = require('./src/builder/init');
+
+    // the runner, well, runs
+    var runner = require('./src/builder/runner');
+
+    init(config, function(initial_queue) {
+
+        function queueJob(job) {
+            for (var i in job) {
+                if (job.hasOwnProperty(i)) {
+                    var spec = job[i].spec ? job[i].spec.name : default_spec;
+                    var git_url = job[i].spec ? job[i].spec.git : null;
+                    var entry = job[i].spec ? job[i].spec.entry : config.app.entry;
+                    job[i].builder = require('./src/builder/builder')(spec, entry, false, git_url);
+                    queue.push(job);
+                }
             }
         }
-    }
 
-    // init build may return an array of initial jobs to queue
-    initial_queue.forEach(queueJob);
+        // init build may return an array of initial jobs to queue
+        initial_queue.forEach(queueJob);
 
-    // start listening
-    runner(queueJob, config);
-});
+        // start listening
+        runner(queueJob, config);
+    });
+}
 
